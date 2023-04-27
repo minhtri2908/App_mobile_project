36a22488327d2fe583985671a2a71730a59f2740
Phạm Minh Trí <91834683+minhtri2908@users.noreply.github.com>
2023-04-28 01:50:51 +0700

    update home

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,44 +7,118 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    BottomNavigationView bottomNavigationView;
+//    HomeFragment homeFragment = new HomeFragment();
+//    SearchFragment searchFragment = new SearchFragment();
+//    SettingFragment settingFragment = new SettingFragment();
+//    UserProfileFragment userProfileFragment = new UserProfileFragment();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        bottomNavigationView = findViewById(R.id.bottom_navigation);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+//        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                    case R.id.home:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+//                        return true;
+//                    case R.id.user_profile:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, userProfileFragment).commit();
+//                        return true;
+//                    case R.id.search:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
+//                        return true;
+//                    case R.id.setting:
+//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingFragment).commit();
+//                        return true;
+//                }
+//                return false;
+//            }
+//        });
+//    }
+//
+//}
 public class MainActivity extends AppCompatActivity {
-
-    BottomNavigationView bottomNavigationView;
-    HomeFragment homeFragment = new HomeFragment();
-    SearchFragment searchFragment = new SearchFragment();
-    SettingFragment settingFragment = new SettingFragment();
-    UserProfileFragment userProfileFragment = new UserProfileFragment();
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        viewPager = findViewById(R.id.container);
+
+        // Tạo adapter cho ViewPager
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+
+        // Đặt listener cho BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-                        return true;
-                    case R.id.user_profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, userProfileFragment).commit();
+                        viewPager.setCurrentItem(0);
                         return true;
                     case R.id.search:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
+                        viewPager.setCurrentItem(1);
+                        return true;
+                    case R.id.user_profile:
+                        viewPager.setCurrentItem(2);
                         return true;
                     case R.id.setting:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingFragment).commit();
+                        viewPager.setCurrentItem(3);
                         return true;
+                    default:
+                        return false;
                 }
-                return false;
             }
         });
     }
 
+    private static class PagerAdapter extends FragmentPagerAdapter {
+        private static final int NUM_PAGES = 4;
+
+        public PagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new HomeFragment();
+                case 1:
+                    return new SearchFragment();
+                case 2:
+                    return new UserProfileFragment();
+                case 3:
+                    return new SettingFragment();
+                default:
+                    return null;
+            }
+        }
+    }
 }
