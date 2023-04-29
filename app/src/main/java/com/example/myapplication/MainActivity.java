72fa58e9c259e864b270685969a899db95c2fed4
Phạm Minh Trí <91834683+minhtri2908@users.noreply.github.com>
2023-04-29 15:42:36 +0700

    commit login and register page, update user profile page

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,46 +15,12 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    BottomNavigationView bottomNavigationView;
-//    HomeFragment homeFragment = new HomeFragment();
-//    SearchFragment searchFragment = new SearchFragment();
-//    SettingFragment settingFragment = new SettingFragment();
-//    UserProfileFragment userProfileFragment = new UserProfileFragment();
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        bottomNavigationView = findViewById(R.id.bottom_navigation);
-//        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-//        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.home:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
-//                        return true;
-//                    case R.id.user_profile:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, userProfileFragment).commit();
-//                        return true;
-//                    case R.id.search:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, searchFragment).commit();
-//                        return true;
-//                    case R.id.setting:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingFragment).commit();
-//                        return true;
-//                }
-//                return false;
-//            }
-//        });
-//    }
-//
-//}
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
+    static HomeFragment homeFragment = new HomeFragment();
+    static SearchFragment searchFragment = new SearchFragment();
+    static SettingFragment settingFragment = new SettingFragment();
+    static UserProfileFragment userProfileFragment = new UserProfileFragment();
     private ViewPager viewPager;
 
     @Override
@@ -109,13 +75,13 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return new HomeFragment();
+                    return homeFragment;
                 case 1:
-                    return new SearchFragment();
+                    return searchFragment;
                 case 2:
-                    return new UserProfileFragment();
+                    return userProfileFragment;
                 case 3:
-                    return new SettingFragment();
+                    return settingFragment;
                 default:
                     return null;
             }
---
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Register extends AppCompatActivity {
+
+    EditText username;
+    EditText name;
+    EditText phone;
+    EditText password;
+
+    Button signupreg;
+
+    TextView logintext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        name = findViewById(R.id.name);
+        phone = findViewById(R.id.phone);
+        signupreg = findViewById(R.id.signupButton);
+        logintext = findViewById(R.id.loginText);
+        signupreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        logintext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
